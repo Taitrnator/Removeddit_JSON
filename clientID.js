ea3838f3f611e97be372743c7cd2e4320800eb91
Tait Wayland <taitwayland@Taits-MacBook-Pro-2.local>
2018-02-04 20:27:48 -0500

    checking in an empty clientID file, soon to be gitignored

--- clientID.js
+++
@@ -1,8 +0,0 @@
-// replace this with your oath token
-export const credentials = {
-userAgent: 'Your User Agent',
-clientId: 'Your Client ID',
-clientSecret: 'your secret key',
-username: 'your username',
-password: 'your password'
-}
