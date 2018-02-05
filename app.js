6a009d6451f8bdeeb6f1a4488a1143a19080ddfc
Tait Wayland <taitwayland@Taits-MBP-2.wv.cc.cmu.edu>
2018-02-05 11:14:32 -0500

    added pushshift utilities

--- app.js
+++ app.js
@@ -1,14 +1,58 @@
-import {getComments} from './comments';
-import {getThread} from './thread';
+import fs from 'fs';
 import snoowrap from 'snoowrap'
+import {  getPost,
+          getCommentIDs } from './comments';
+import {getThread} from './thread';
+
 import { Token } from './token';
-const fs = require('fs');
+import {
+  getPost as getRemovedPost,
+  getCommentIDs as getAllCommentIDs,
+  getComments as getRemovedComments,
+} from './pushshift';
+import { isDeleted, difference } from './utils';
+
 
 // getComments('TwoXChromosomes', '7v6xf3');
 // let subreddit = Token.getSubreddit('TwoXChromosomes')
 
 // Token.getHot().then(console.log);
 
-getThread('TwoXChromosomes', '7v6xf3').then(value => {
-  fs.writeFile('thread.json', value, function(err){ });
-})
+// getThread('TwoXChromosomes', '7v6xf3').then(value => {
+//   fs.writeFile('thread.json', value, function(err){ });
+// })
+
+
+let subreddit = 'TwoXChromosomes',
+    threadID = '7v6xf3';
+
+    console.timeEnd('scripts loaded')
+
+    Promise.all([
+      // Get thread from reddit
+      getPost(subreddit, threadID)
+        .then(post => {
+          // Fetch the thread from pushshift if it was deleted/removed
+          if (isDeleted(post.selftext)) {
+            console.log('post deleted');
+            // getRemovedPost(threadID)
+            //   .then(removedPost => {
+            //     removedPost.removed = true
+            //     this.setState({ post: removedPost })
+            //   })
+          }
+        })
+      // Get comment ids from reddit
+        .then(() => getCommentIDs(subreddit, threadID)),
+
+      // Get comment ids from pushshift
+      getAllCommentIDs(threadID)
+])
+      .then(results => {
+        const foundIDs = results[0]
+        const allIDs = results[1]
+
+        const removedIDs = difference(allIDs, foundIDs)
+        // Get removed comments from pushshift
+        return getRemovedComments(removedIDs)
+      })
